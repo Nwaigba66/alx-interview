0ec8abdb13c1ae3932bf96c51228c33768e85f57
Nwaigba66 <glorianwaigba@gmail.com>
2023-12-13 23:33:48 +0000

    Added 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,7 +2,7 @@ def minOperations(n):
     if n == 1:
         return 0
 
-    # Initialize an array to store the minimum operations needed for each position
+    # Initialize an array to store the minimum operations
     dp = [float('inf')] * (n + 1)
     dp[1] = 0
 
@@ -10,12 +10,10 @@ def minOperations(n):
         # Check all possible factors of i
         for j in range(2, i + 1):
             if i % j == 0:
-                # Update the minimum operations needed for position i based on previous positions
+                # Update the minimum operations
                 dp[i] = min(dp[i], dp[j] + i // j)
-
     return dp[n] if dp[n] != float('inf') else 0
-
 # Example usage
-n = 9
-result = minOperations(n)
-print(f"Number of operations for {n} H characters: {result}")
+    n = 9
+    result = minOperations(n)
+    print(f"Number of operations for {n} H characters: {result}")
