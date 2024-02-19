04be6bc7f2c0e0b88cbbcce7f8eae246db519754
Gloria Nwaigba <glorianwaigba@gmail.com>
2024-02-19 08:07:25 +0000

    created all files codebase

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,24 +3,38 @@
 """
 
 
-def is_prime(num):
-    if num < 2:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
 def isWinner(x, nums):
-    ben_wins = 0
-    for n in nums:
-        if is_prime(n):
-            ben_wins += 1
+    """Define the Prime game
+    """
 
-    # Determine the winner based on who won the most rounds
-    if ben_wins > x // 2:
-        return "Ben"
-    elif ben_wins < x // 2:
+    def sieve_of_eratosthenes(n):
+        primes = [True] * (n + 1)
+        primes[0] = primes[1] = False
+        for i in range(2, int(n ** 0.5) + 1):
+            if primes[i]:
+                for j in range(i * i, n + 1, i):
+                    primes[j] = False
+        return [i for i in range(n + 1) if primes[i]]
+
+    def can_win(n):
+        primes = sieve_of_eratosthenes(n)
+        if n in primes:
+            return True
+        return False
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if can_win(n):
+            if n % 2 == 0:
+                maria_wins += 1
+            else:
+                ben_wins += 1
+
+    if maria_wins > ben_wins:
         return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
     else:
         return "Ben"  # If equal wins, declare Ben as the winner
