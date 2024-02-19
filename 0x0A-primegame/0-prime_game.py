fcb8ea7b30a7d189fc8d3d1b8bfbe5111b636c00
Gloria Nwaigba <glorianwaigba@gmail.com>
2024-02-19 07:49:10 +0000

    Updated codebase

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,6 +2,7 @@
 """This Module defines the isWinner and other helper functions
 """
 
+
 def is_prime(num):
     if num < 2:
         return False
@@ -11,17 +12,18 @@ def is_prime(num):
     return True
 
 def isWinner(x, nums):
-    maria_wins = 0
+    ben_wins = 0
     for n in nums:
         if n == 1 or n % 2 == 0:
-            # If n is 1 or even, Ben wins directly
+            # If n is 1 or even, Maria wins directly
             continue
-        maria_wins += 1
-    
+        ben_wins += 1
+
     # Determine the winner based on who won the most rounds
-    if maria_wins > x // 2:
-        return "Maria"
-    elif maria_wins < x // 2:
+    if ben_wins > x // 2:
         return "Ben"
+    elif ben_wins < x // 2:
+        return "Maria"
     else:
         return None
+
