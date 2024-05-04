fc1bdc8fed85b37125e80234c68db8ee10cac13c
Nwaigba66 <your.email@glorianwaigba@gmail.com>
2024-05-04 13:21:49 +0200

    Interview coding challenge

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,20 +1,43 @@
 #!/usr/bin/python3
-"""This module define a single function makeChange
 """
-from typing import List
+Change comes from within
+"""
 
 
-def makeChange(coins: List[int], total: int) -> int:
-    """Finds the minimum number of coins to make a target total
+def makeChange(coins, total):
+    """
+    Return the minimum number of coins needed to meet a given total
+    Args:
+        coins (list of ints): a list of coins of different values
+        total (int): total value to be met
+    Return:
+        Number of coins or -1 if meeting the total is not possible
     """
     if total <= 0:
         return 0
+    if coins == [] or coins is None:
+        return -1
+    try:
+        n = coins.index(total)
+        return 1
+    except ValueError:
+        pass
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for coin in coins:
-        for sub_value in range(coin, total + 1):
-            dp[sub_value] = min(dp[sub_value], dp[sub_value - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+    coins.sort(reverse=True)
+    coin_count = 0
+    for i in coins:
+        if total % i == 0:
+            coin_count += int(total / i)
+            return coin_count
+        if total - i >= 0:
+            if int(total / i) > 1:
+                coin_count += int(total / i)
+                total = total % i
+            else:
+                coin_count += 1
+                total -= i
+                if total == 0:
+                    break
+    if total > 0:
+        return -1
+    return coin_count
--- 0x08-making_change/README.md
+++ 0x08-making_change/README.md
@@ -7,4 +7,39 @@
 - The first line of all your files should be exactly #!/usr/bin/python3
 - A README.md file, at the root of the folder of the project, is mandatory
 - Your code should use the PEP 8 style (version 1.7.x)
-- All your files should be executable
+- All your files should be executable.
+
+#Tasks
+# Change comes from within
+
+
+#Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total.
+
+-Prototype: def makeChange(coins, total)
+-Return: fewest number of coins needed to meet total
+-If total is 0 or less, return 0
+-If total cannot be met by any number of coins you have, return -1
+-coins is a list of the values of the coins in your possession
+-The value of a coin will always be an integer greater than 0
+-You can assume you have an infinite number of each denomination of coin in the list
+-Your solution’s runtime will be evaluated in this task
+
+#/0x08-making_change$ cat 0-main.py
+
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+makeChange = __import__('0-making_change').makeChange
+
+print(makeChange([1, 2, 25], 37))
+
+print(makeChange([1256, 54, 48, 16, 102], 1453))
+
+#0x08-making_change$
+#0x08-making_change$ ./0-main.py
+- 7
+- -1
+
+- 0x08-making_change$
